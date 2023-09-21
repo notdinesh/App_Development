d1f53db4b75e883ecf382d246708207395ef7098
DINESH R <112968567+notdinesh@users.noreply.github.com>
2023-09-21 14:37:39 +0530

    Add files via upload

---
+++ Day 7/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Login from './Login';
+import Profile from './Profile';
+import ColorSelector from './ColorSelector';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile />
+      <Login />
+      <ColorSelector />
+    </div>
+  );
+}
+
+export default App;
