b7045a626257993948c8f171f65b7445d381bb78
DINESH R <112968567+notdinesh@users.noreply.github.com>
2023-10-11 09:25:31 +0530

    Delete Day 17/Custdetail/Custdetail directory

--- Day 17/Custdetail/Custdetail/src/main/java/com/example/demo/Repository/SongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-import com.example.demo.Entity.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SongRepository extends JpaRepository<Song, Long> {
-    // You can add custom query methods here if needed
-}
